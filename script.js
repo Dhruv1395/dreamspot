0e0963f7b45148b45648057d11edd4664a1b8490
Dhruv1395 <142381298+Dhruv1395@users.noreply.github.com>
2024-06-04 16:50:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ close.addEventListener("click",function(){
     tl.reverse();
     
 })
+
+        let day = document.getElementById("day");
+        let hrs = document.getElementById("hrs");
+        let min = document.getElementById("min");
+        let sec = document.getElementById("sec");
+        hrs = hrs>12? hrs % 12 : hrs;
+        
+       
+        setInterval(()=>{
+            var ctime = new Date()
+            hrs.innerHTML =  ctime.getHours();
+            min.innerHTML = (ctime.getMinutes()<10?"0":"") +  ctime.getMinutes();
+            sec.innerHTML =ctime.Seconds();
+            day.innerHTML=ctime.getDate();
+        },1000)
